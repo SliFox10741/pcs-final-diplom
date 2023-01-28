93617149105abde0725b0ff76794e74e162972de
Maxim <mr.maximgus@gmail.com>
2023-01-28 10:34:01 +0500

    Примерное заполнение списка словами

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,36 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+//        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
+//        System.out.println(engine.search("бизнес"));
+
+        List<String> a = new ArrayList<>();
+        File pdf = new File("pdfs/1. DevOps_MLops.pdf");
+        var doc = new PdfDocument(new PdfReader(pdf));
+
+
+        String text;
+        String[] words;
+        int pages = doc.getNumberOfPages();
+        for (int i = 1; i <= pages; i++){
+            var page = doc.getPage(i);
+            text = PdfTextExtractor.getTextFromPage(page);
+            words = text.split("\\P{IsAlphabetic}+");
+            for (String word : words) {
+                if (!a.contains(word.toLowerCase())) {
+                    a.add(word.toLowerCase());
+                }
+            }
+        }
+        System.out.println(a);
 
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
